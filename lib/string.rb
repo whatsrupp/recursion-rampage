c4db878548aa65e6f031b2d83d431fa402c66fcc
Nick Rupp <nickrupp95@gmail.com>
2017-06-08 20:34:18 +0100

    get basic addition to work

--- lib/string.rb
+++ lib/string.rb
@@ -1,7 +1,14 @@
 
 class String
-  include Factory
   def objectify
-
+    input = self
+    isAddedRegex = /[^\+]+/
+    filtered = input.scan(isAddedRegex)
+    isDigitRegex = /[\-|\d]/
+    filtered.map! { |entry|  entry.match(isDigitRegex) ?  entry.to_i : entry  }
+    add(filtered)
   end
+
+
+  
 end
