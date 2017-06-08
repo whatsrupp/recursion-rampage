e7a70a4a0f719fe54226e390c9fca37dbb91ad0f
Nick Rupp <nickrupp95@gmail.com>
2017-06-08 23:05:41 +0100

    amend minus-filtering regex

--- lib/string.rb
+++ lib/string.rb
@@ -4,18 +4,19 @@ class String
     input = self
     add_regex = /[^\+]+/
     digit_regex = /[\-|\d]/
-    minus_regex = /(\-.*?(?=-))|(\-.*)/
+    minus_regex = /(?:\-.*?(?=-))|(?:\-.*)/
 
     negatives = !!/\-/.match(input)
 
     filter_out_plus = input.scan(add_regex)
     if negatives
-      filter_out_plus.map! { |e|  !!/\-/.match(e)? e.scan(minus_regex).flatten.compact : e }.flatten!
+      quicktest = filter_out_plus.map! { |e|  !!/\-/.match(e)? e.scan(minus_regex) : e }.flatten!
     end
     filter_out_plus.map! { |entry|  entry.match(digit_regex) ?  entry.to_i : entry  }
 
     add(filter_out_plus)
   end
+
 
 
 
