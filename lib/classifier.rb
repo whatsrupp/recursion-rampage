f27421c6cbabd30b307cfcd738b1d7b59567fe3f
Nick Rupp <nickrupp95@gmail.com>
2017-06-13 20:25:08 +0200

    Add method to extract strings from between brackets

--- lib/classifier.rb
+++ lib/classifier.rb
@@ -1,7 +1,83 @@
 module Classifier
 
   def classify(expression_string)
-    return :div
+    return :pow if power_expression?(expression_string)
+    return :div if division_expression?(expression_string)
+    return :mult if multiplication_expression?(expression_string)
+    return :add if addition_expression?(expression_string)
+  end
+
+
+  def power_expression?(expression_string)
+    false
+  end
+
+  def division_expression?(expression_string)
+    # Criteria: Starts with \frac, ends with }
+    #Unsolved: how do you contain everything within a regex where there are multiple
+    #Make a test string-> get the maximum length total match
+
+    #starts with \frac{
+    starting_frac_reg = /^\\frac{/
+    starts_with_frac = !!(starting_frac_reg =~ expression_string)
+    #ends with }
+    ending_curly_reg = /}$/
+    ends_with_curly_bracket = !!(ending_curly_reg =~ expression_string)
+    #Greedily consume the entire expression:
+    #Capture group is only between the two outermost curly brackets
+    between_curly_reg = /\\frac{(.+)}/
+    #Work Recursively to see if there is not a division involved
+    frac_covers_entire_expression = true
+    #Not sure how to do this properly but would need to be included if expanded
+
+    starts_with_frac || ends_with_curly_bracket || frac_covers_entire_expression
+  end
+
+  def multiplication_expression?(expression_string)
+    #3) Is it a Multiplication?
+    # Criteria:
+    # Letter next to number: a2
+    # multiple letters: aa2, ab3
+    # number next to letter: 2a
+    # multi-digit number and letter: 22a
+    # combination: 22a22b
+    # optional minus sign: -a2 / -a2
+    # number or letter next to brackets a(), 2()
+    # with optional minus -a(), 2()
+
+  end
+
+  def addition_expression?(expression_string)
+    false
+  end
+
+  def extract_string_from_outer_bracket(string)
+    bracket_count = 0
+    bracket_start_index = nil
+
+    string.each_char.with_index do |char,index|
+      if (char=='(')
+        if bracket_count == 0
+          bracket_start_index = index
+        end
+        bracket_count += 1
+      end
+      if (char==')')
+        bracket_count-=1
+        if bracket_count == 0
+          bracket_end_index = index
+          length = bracket_end_index - bracket_start_index
+          bracket_string = string[bracket_start_index+1,bracket_end_index-bracket_start_index-1]
+          # output = {
+          #   bracket_string: bracket_string,
+          #   start_index: bracket_start_index,
+          #   end_index: bracket_end_index
+          # }
+          return bracket_string
+        end
+      end
+    end
+    return string
 
   end
 end
--- spec/lib/classifier_spec.rb
+++ spec/lib/classifier_spec.rb
@@ -11,14 +11,45 @@ describe 'Classifier' do
     test_string = '\frac{-14a}{2+x}'
     expect(classifier.classify(test_string)).to eq(:div)
   end
-  it ' \frac{-14a}{2+x}' do
+  it '3(x+7)+4' do
     test_string = '3(x+7)+4'
     expect(classifier.classify(test_string)).to eq(:add)
   end
-  # it '2(x+\frac{3}{y})w' do
-  #   test_string = '2(x+\frac{3}{y})w'
-  #   expect(classifier.classify(test_string)).to eq(:mult)
-  # end
+  it '2(x+\frac{3}{y})w' do
+    test_string = '2(x+\frac{3}{y})w'
+    expect(classifier.classify(test_string)).to eq(:mult)
+  end
+
+
+  describe '#extract_string_from_outer_bracket' do
+
+    it 'returns the string between brackets' do
+      test_string = '(2x+3)'
+      result =classifier.extract_string_from_outer_bracket(test_string)
+      expect(result).to eq ('2x+3')
+
+
+    end
+
+    it 'returns the string if no brackets' do
+      test_string = '322x+3'
+      result =classifier.extract_string_from_outer_bracket(test_string)
+      expect(result).to eq ('322x+3')
+    end
+
+    it 'finds the first bracket' do
+      test_string = '32(2x+3)'
+      result =classifier.extract_string_from_outer_bracket(test_string)
+      expect(result).to eq ('2x+3')
+    end
+
+    it 'returns the string between nested brackets' do
+      test_string = '32(2(x+3))'
+      result =classifier.extract_string_from_outer_bracket(test_string)
+      expect(result).to eq ('2(x+3)')
+
+    end
+  end
   # it '\frac{21a+3}{\frac{-14a}{2+x}-(b+d)}' do
   #   test_string = '\frac{21a+3}{\frac{-14a}{2+x}-(b+d)}'
   #   expect(classifier.classify(test_string)).to eq(:frac)
@@ -31,4 +62,5 @@ describe 'Classifier' do
   #   test_string = '2(x+\frac{3}{y})w'
   #   expect(classifier.classify(test_string)).to eq(:mult)
   # end
+
 end
