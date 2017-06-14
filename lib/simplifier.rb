66e53509f1d5bd067400a3952b9e5ab11c7fbfef
Nick Rupp <nickrupp95@gmail.com>
2017-06-14 12:18:33 +0100

    Refactor simplify to be a disposable class

--- lib/simplifier.rb
+++ lib/simplifier.rb
@@ -1,49 +1,65 @@
 class Simplify
 
-  def expression(string)
-    fractions(string)
-    parentheses(string)
-    multiplications(string)
+  attr_reader :replaced_content
+  attr_reader :string
+
+  def initialize (string)
+    @replaced_content = []
+    @string = string
   end
 
-  def multiplications(string)
+  def expression
+    fractions
+    parentheses
+    multiplications
+  end
+
+  def multiplications
     non_bracket_mult_regex = /(?:[0-9$]+[a-zA-Z$](?:[\w]*))|(?:[a-zA-Z$]+[0-9$]+(?:[\w]*))/
     string.gsub!(non_bracket_mult_regex, '(\0)')
-    parentheses(string)
+    parentheses
   end
 
-  def fractions (string)
-    simplified_bracket_string = fraction_brackets(string)
+  def fractions
+    simplified_bracket_string = fraction_brackets
     simplified_bracket_string.gsub!('\frac{$}{$}', '$')
+    store_matches('\frac{$}{$}')
     more_fractions = !!(/\\frac{/ =~ simplified_bracket_string)
-    return fractions(simplified_bracket_string) if more_fractions
+    return fractions if more_fractions
     simplified_bracket_string
+
   end
 
-  def fraction_brackets(string)
-    first_bracket_string = extract_string_between_curly_bracket(string)
+  def store_matches(matched_string)
+    @replaced_content << matched_string
+  end
+
+  def fraction_brackets
+    first_bracket_string = extract_string_between_curly_bracket
+    store_matches(first_bracket_string)
     first_bracket_string = '\frac'+first_bracket_string
     test = string.sub(first_bracket_string, '\frac$')
     string.sub!(first_bracket_string, '\frac$')
-    second_bracket_string = extract_string_between_curly_bracket(string)
+    second_bracket_string = extract_string_between_curly_bracket
+    store_matches(second_bracket_string)
     string.sub!(second_bracket_string, '$')
     string.gsub!('\\frac$$', '\\frac{$}{$}')
   end
 
-  def parentheses(string)
-    simplified_string = parentheses_content(string)
+  def parentheses
+    simplified_string = parentheses_content
     string.gsub!('($)','$')
     more_parentheses = !!(/\(/ =~ string)
-    return parentheses(simplified_string) if more_parentheses
+    return parentheses if more_parentheses
     simplified_string
   end
 
-  def parentheses_content(string)
-    inner_string = extract_string_from_outer_bracket(string)
+  def parentheses_content
+    inner_string = extract_string_from_outer_bracket
     string.sub!(inner_string, '($)')
   end
 
-  def extract_string_from_outer_bracket(string)
+  def extract_string_from_outer_bracket
     bracket_count = 0
     bracket_start_index = nil
 
@@ -69,7 +85,7 @@ class Simplify
   end
 
 
-  def extract_string_between_curly_bracket(string)
+  def extract_string_between_curly_bracket
     bracket_count = 0
     bracket_start_index = nil
 
--- spec/lib/simplifier_spec.rb
+++ spec/lib/simplifier_spec.rb
@@ -2,86 +2,87 @@ require_relative '../spec_helper'
 
 describe 'Simplifier' do
 
-  let (:simplify) {Simplify.new()}
   describe '#fraction_brackets' do
     it 'simplifies a top level fraction into a dollar sign' do
-      test_string = '\frac{a}{b}'
+      simplify = Simplify.new('\frac{a}{b}')
       result = '\frac{$}{$}'
-      expect(simplify.fraction_brackets(test_string)).to eq result
+      binding.pry
+      expect(simplify.fraction_brackets).to eq result
     end
 
     it 'simplifies nested fractions' do
-      test_string = '\frac{\frac{a}{b}}{a}'
+      simplify = Simplify.new('\frac{\frac{a}{b}}{a}')
       result = '\frac{$}{$}'
-      expect(simplify.fraction_brackets(test_string)).to eq result
+      expect(simplify.fraction_brackets).to eq result
     end
   end
 
   describe '#fractions' do
     it 'simplifies entire fraction with no nesting' do
-      test_string = '\frac{b}{a}'
+      simplify = Simplify.new('\frac{b}{a}')
       result = '$'
-      expect(simplify.fractions(test_string)).to eq result
+      expect(simplify.fractions).to eq result
     end
     it 'simplifies entire fraction with nesting' do
-      test_string = '\frac{\frac{a}{b}}{a}'
+      simplify = Simplify.new('\frac{\frac{a}{b}}{a}')
       result = '$'
-      expect(simplify.fractions(test_string)).to eq result
+      expect(simplify.fractions).to eq result
     end
     it 'simplifies non-nested fractions' do
-      test_string = '\frac{a}{a}\frac{a}{b}'
+      simplify = Simplify.new('\frac{a}{a}\frac{a}{b}')
       result = '$$'
-      expect(simplify.fractions(test_string)).to eq result
+      expect(simplify.fractions).to eq result
+      binding.pry
     end
   end
 
   describe '#parentheses_content' do
     it 'simplifies the contents of parentheses' do
-      test_string = '2(a)b'
+      simplify = Simplify.new('2(a)b')
       result = '2($)b'
-      expect(simplify.parentheses_content(test_string)).to eq result
+      expect(simplify.parentheses_content).to eq result
     end
     it 'simplifies the contents of parentheses with nesting' do
-      test_string = '2(a(a(b)(sx)))b'
+      simplify = Simplify.new('2(a(a(b)(sx)))b')
       result = '2($)b'
-      expect(simplify.parentheses_content(test_string)).to eq result
+      expect(simplify.parentheses_content).to eq result
     end
   end
 
   describe '#parentheses' do
     it 'simplifies the entire parentheses' do
-      test_string = '2(a)b'
+      simplify = Simplify.new('2(a)b')
       result = '2$b'
-      expect(simplify.parentheses(test_string)).to eq result
+      expect(simplify.parentheses).to eq result
 
     end
 
     it 'simplifies multiple parentheses' do
-      test_string = '2(a)(b)+1(a)'
+      simplify = Simplify.new('2(a)(b)+1(a)')
       result = '2$$+1$'
-      expect(simplify.parentheses(test_string)).to eq result
+      expect(simplify.parentheses).to eq result
     end
   end
 
   describe 'multiplications' do
     it 'simplifies basic multipliers' do
-      test_string = '2ab+3a'
+      simplify = Simplify.new('2ab+3a')
       result = '$+$'
-      expect(simplify.multiplications(test_string)).to eq result
+      expect(simplify.multiplications).to eq result
     end
 
     it 'simplifies the replacement symbol' do
-      test_string = '2$a+3$$$a'
+      simplify = Simplify.new('2$a+3$$$a')
       result = '$+$'
-      expect(simplify.multiplications(test_string)).to eq result
+      expect(simplify.multiplications).to eq result
     end
   end
 
   describe '#expression' do
     it 'handles mixed stacks ' do
-      test_string = '2\frac{}{}(\frac{a}{b})+1(a)'
+      simplify = Simplify.new('2\frac{}{}(\frac{a}{b})+1(a)')
       result = '$+$'
-      expect(simplify.expression(test_string)).to eq result
+      expect(simplify.expression).to eq result
     end
 
     it 'makes a call to the parentheses function' do
